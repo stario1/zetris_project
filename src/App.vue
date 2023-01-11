e52c3769b6c106dd82dc039067cb5b31f35457ea
stario1 <colourmatik811@gmail.com>
2023-01-11 10:08:35 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+
+<g-header/>
+  <g-registration/>
+
+</template>
+
+<script>
+import gHeader from './components/g-header.vue';
+import gRegistration from './components/g-registration.vue';
+
+
+export default {
+  name: 'App',
+  components: {gHeader, gRegistration
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
